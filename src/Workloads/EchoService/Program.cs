da341898c6fab92c4dec4d529f59062951aab853
Alex Lodge <awlodge@gmail.com>
2026-07-12 14:53:01 +0100

    Publish echo service as binary

--- src/Workloads/EchoService/Program.cs
+++ src/Workloads/EchoService/Program.cs
@@ -3,6 +3,7 @@ using Maelstrom;
 
 HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
 builder.Services.AddMaelstromNodeWorkload<EchoServer>();
+builder.Logging.AddConsole(o => o.LogToStandardErrorThreshold = LogLevel.Trace);
 
 using IHost host = builder.Build();
 await host.RunMaelstromNodeAsync();
